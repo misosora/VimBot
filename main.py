b9545f9df4ddaff3d10b95986cce275ab95a25db
gabi <gabisfn@outlook.com>
2020-06-30 01:46:05 -0300

    fixing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, CommandHandler
 import logging
 import random
 
-TOKEN = "1266640579:AAHyj9CNRpFSq1Zimt_u26rN9IQjJRXn6hg"
+from tok.py import *
 
 def start(update, context):
     s = "Olá, @{}! (• ε •)".format(update.effective_user.username)
