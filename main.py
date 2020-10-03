8df71f362d697ade3e3c536369df61f40bcfe231
Dio <diogenes.pedro06@gmail.com>
2020-10-03 16:11:48 -0300

    Adding dio memes function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from telegram.ext import Updater, CommandHandler
 import logging
 import random
 from froggies import froggy_pics
+from dio import dio_pics
 
 def start(update, context):
     s = "Olá, @{}! (• ε •)".format(update.effective_user.username)
@@ -75,12 +76,19 @@ def asterisco(update, context):
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=s)
 
+def dio(update, context):
+    image = "./Dio/"
+    index = random.choice(list(dio_pics.keys()))
+    image += dio_pics[index]["img"]
+    caption = "<i>" + dio_pics[index]["cap"] + "</i>"
+    context.bot.sendPhoto(chat_id=update.message.chat_id, photo=open(image, "rb"), caption=caption, parse_mode="html")
+
 def main():
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s [%(levelname)s] %(message)s")
 
-    updater = Updater(token=TOKEN, use_context=True)
+    updater = Updater(token="1377675743:AAGOPVoJK8GcRnLR6OW8uueIpud4aPNSyao", use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
@@ -91,6 +99,7 @@ def main():
     dp.add_handler(CommandHandler("corona", corona))
     dp.add_handler(CommandHandler("froggypic", froggypic))
     dp.add_handler(CommandHandler("asterisco", asterisco))
+    dp.add_handler(CommandHandler("dio", dio))
 
     updater.start_polling()
     logging.info("=== It's alive! ===")
