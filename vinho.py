770da21a82210f770423d20ebac042a83f1a33dc
guitld <seuto8199@gmail.com>
2020-10-19 14:33:07 -0300

    Added vinho command

---
+++ vinho.py
@@ -0,0 +1,98 @@
+vinhoImages = {
+    "scarecrow": {
+        "img": "scarecrow.jpg",
+        "cap": "Scarecrow Cabernet Sauvignon: Vinho tinto caro demais bixo\nPreço: RTX 3080"
+    },
+    "domaine": {
+        "img": "domaine.jpg",
+        "cap": "La Tâche Grand Cru: Esse aqui é lá da frança rapaz, international wine\nPreço: Celta usado em bom estado (aquele que dispensa curiosos)"
+    },
+    "soldera": {
+        "img": "soldera.jpg",
+        "cap": "Soldera Sangiovese Toscana: Mamma mia vino rosso italiano\nPreço: Roteador Sony Playstation 5"
+    },
+    "sauternes": {
+        "img": "sauternes.jpg",
+        "cap": "Château d’Yquem Sauternes: Aquele vinhozinho branco que cai bonitinho com uma sobremesa ui ui docinho\nPreço: Refrigerador Panasonic Frost Free 2 Portas"
+    },
+    "taylor": {
+        "img": "taylor.jpg",
+        "cap": "Very Old Single Harvest Port: Vinho tinto de 1968, mais velho que meu pai. Garrafa preta fosca lindona\nPreço: Ryzen 5 3600"
+    },
+    "cru": {
+        "img": "cru.jpg",
+        "cap": "Grands-Échezeaux Grand Cru: Aquele tinto francês que deixa um gostinho de chocolate nestlé hmm\nPreço: Todo vinho francês pode se equiparar com um carro"
+    },
+    "chadwick": {
+        "img": "chadwick.jpg",
+        "cap": "Red de Viñedo Chadwick: R.I.P Chadwick Boseman (⌯˃̶᷄ ﹏ ˂̶᷄⌯)ﾟ\nPreço: Roupa cosplay Pantera Negra"
+    },
+    "tosca": {
+        "img": "tosca.jpg",
+        "cap": "Masseto Toscana: O nome desse aí ficou meio estranho...\nPreço: 275kg de linguiça toscana Aurora"
+    },
+    "pera": {
+        "img": "pera.jpg",
+        "cap": "Cartuxa Pêra-Manca: Propaganda enganosa, nem é feito de pera\nPreço: Volkswagen Gol GT 1.8 1984"
+    },
+    "petrus": {
+        "img": "petrus.jpg",
+        "cap": "Pétrus Pomerol: Aquele vinho seco. Curto e grosso\nPreço: Uma casa. Sim, uma casa"
+    },
+    "violeta": {
+        "img": "violeta.jpg",
+        "cap": "Monteviejo Lindaflor La Violeta Malbec: É boludo, seu organismo não merece ele\nPreço: 32gb de ram"
+    },
+    "barca": {
+        "img": "barca.jpg",
+        "cap": "Barca Velha Douro da Casa Ferreirinha: Bixo... casa ferreirinha é de fude\nPreço: Climatizador Philco PCL1F Ventila, Umidifica, Purifica e Evapora com 3 Velocidades"
+    },
+    "acre": {
+        "img": "acre.jpg",
+        "cap": "Hundred Acre Kayli Morgan Vineyard Cabernet Sauvignon: Vinho estadunidense, tome por sua conta e risco\nPreço: Geladeira Microsoft Xbox Series X"
+    },
+    "minuty": {
+        "img": "minuty.jpg",
+        "cap": "Minuty Prestige: Aquele rosézinho perfeito pro calor\nPreço: Boneca LOL Surprise Furniture Bredoom Candide"
+    },
+    "unico": {
+        "img": "unico.jpg",
+        "cap": "Vega Sicilia Unico: Apesar do nome, ele não é o único vinho tinto no mercado\nPreço: Smart TV Crystal UHD 4K LED 75” Samsung com 3 HDMI e 2 USB"
+    },
+    "chapinha": {
+        "img": "chapinha.jpg",
+        "cap": "Vinho Chapinha: O mais brabo de todos, sem mais\nPreço: 11 garrafinhas de detergente Ypê Capim-Limão 500ml"
+    },
+    "chapa": {
+        "img": "chapa.jpg",
+        "cap": "Vinho Para o meu Chapa: Excelente presente de amigo secreto\nPreço: Álcool Gel 70% extrato de algodão 500 ml Hi Clean"
+    },
+    "suave": {
+        "img": "suave.jpg",
+        "cap": "Vinho Goes Bordô Suave: Aquele tranquilo, gente boníssima\nPreço: Pano de prato microfibra Esfrebom branco com estampa"
+    },
+    "lambrusco": {
+        "img": "lambrusco.jpg",
+        "cap": "Vinho Lambrusco: Parece xarope\nPreço: Registro de pressão 1416 3/4 C-40"
+    },
+    "azul": {
+        "img": "azul.jpg",
+        "cap": "Vinho Azul: Parece amaciante, não recomendo\nPreço: Capacete para moto aberto preto fosco"
+    },
+    "vale": {
+        "img": "vale.jpg",
+        "cap": "Vinho Cantinho do Vale: Clássico do bar. Se for tomar, tome muito pra esquecer\nPreço: Ice Age 3 Dawn of the Dinosaurs Playstation 2 (frete único)"
+    },
+    "saobernardo": {
+        "img": "saobernardo.jpg",
+        "cap": "Vinho São Bernardo: Vinho de cachorro salva-vidas\nPreço: Borracha Artística Faber-Castell Maleável Cinza"
+    },
+    "pinheirense": {
+        "img": "pinheirense.jpg",
+        "cap": "Vinho Pinheirense: É um coquete de vinho na real\nPreço: Trident Watermellon 18 unidades (chicletes na caixa)"
+    },
+    "mateus": {
+        "img": "mateus.jpg",
+        "cap": "Vinho Mateus Rosé: Vinho cristão\nPreço: Ola Lava-Roupas Delicado Sabor Bebê 1L"
+    },
+}
